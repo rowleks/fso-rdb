70a58c96670613a5a3355c057e106b3bcfb6ed55
Rowland Momoh <momohrowland@gmail.com>
2026-04-23 20:26:07 +0100

    Add ex 2.3 (cli.js)

---
+++ cli.js
@@ -0,0 +1,18 @@
+require('dotenv').config()
+const { sequelize } = require('./database')
+
+const main = async () => {
+  try {
+    await sequelize.authenticate()
+    const blogs = await sequelize.query('SELECT * FROM blogs', {
+      type: sequelize.QueryTypes.SELECT,
+    })
+    for (const blog of blogs) {
+      console.log(`${blog.author}: '${blog.title}', ${blog.likes} likes`)
+    }
+  } catch (err) {
+    console.error('DB connection failed:', err)
+  }
+}
+
+main()
